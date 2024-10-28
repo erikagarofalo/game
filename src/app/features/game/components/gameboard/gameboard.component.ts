048be247dc30e91a59dc1580cbdffed3c615a6da
erikagarofalo <erika.garofalo@gmail.com>
2024-10-28 15:03:53 +0100

    struttura base

---
+++ src/app/features/game/components/gameboard/gameboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gameboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './gameboard.component.html',
+  styleUrl: './gameboard.component.css'
+})
+export class GameboardComponent {
+
+}
